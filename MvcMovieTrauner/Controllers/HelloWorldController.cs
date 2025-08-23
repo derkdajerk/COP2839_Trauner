2d035ec5df3066205357d04d51860a4a10b49172
Derek Trauner <derektrauner@gmail.com>
2025-08-22 22:02:08 -0700

    Initialize and upload Module 1 project, Movie MVC App.

---
+++ MvcMovieTrauner/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovieTrauner.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /HelloWorld/ goes to /HelloWorld/Index directly
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MvcMovieTrauner/Data/MvcMovieTraunerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieTrauner.Models;
+
+namespace MvcMovieTrauner.Data
+{
+    public class MvcMovieTraunerContext : DbContext
+    {
+        public MvcMovieTraunerContext (DbContextOptions<MvcMovieTraunerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieTrauner.Models.Movie> Movie { get; set; } = default!;
+    }
+}
