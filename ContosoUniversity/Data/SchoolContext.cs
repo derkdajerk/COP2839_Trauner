f01983be011254075267e1defec75b27b18272b7
Derek Trauner <derektrauner@gmail.com>
2025-09-13 12:39:33 -0700

    Finishing up CRUD operations using entity frameworks, seeding student data into SQL table.

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+
+        public DbSet<Enrollment> Enrollment { get; set; }
+
+        public DbSet<Course> Course { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
